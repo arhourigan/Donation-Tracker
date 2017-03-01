a9e477372da84c6dba561ff9e4202178ef5c3e92
arhourigan <austin.hourigan@gmail.com>
2017-03-01 15:04:06 -0600

    Update Donation Tracker.py

--- Donation Tracker.py
+++ Donation Tracker.py
@@ -5,6 +5,7 @@ page = requests.get('http://www.ppaction.org/site/TR?px=9693618&fr_id=1160&pg=pe
 tree = html.fromstring(page.content)
 #This will find the goal amount
 goal = tree.xpath('//span[@class="amount-raised-value"]/text()')
+goal = [item.replace("\n", "") for item in goal]
 #This prints the currently raised amount out of the total goal
 with open("donation.txt", "w") as text_file:
     print(goal,'/$10000', sep="", file=text_file)
