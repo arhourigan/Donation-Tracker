d02853c150c152f119ac1a81b2f5199904b76050
arhourigan <austin.hourigan@gmail.com>
2017-03-01 16:51:35 -0600

    Update Donation Tracker.py

--- Donation Tracker.py
+++ Donation Tracker.py
@@ -1,11 +1,15 @@
 from lxml import html
 import requests
+import threading
 #Campaign URL goes here
-page = requests.get('http://www.ppaction.org/site/TR?px=9693618&fr_id=1160&pg=personal')
-tree = html.fromstring(page.content)
-#This will find the goal amount
-goal = tree.xpath('//span[@class="amount-raised-value"]/text()')
-goal = [item.replace("\n", "") for item in goal]
-#This prints the currently raised amount out of the total goal
-with open("donation.txt", "w") as text_file:
-    print("".join(map(str, goal)), '/$10,000', sep="", file=text_file)
+def timeloop():
+    threading.Timer(60.0, timeloop).start()
+    page = requests.get('http://www.ppaction.org/site/TR?px=9693618&fr_id=1160&pg=personal')
+    tree = html.fromstring(page.content)
+    #This will find the goal amount
+    goal = tree.xpath('//span[@class="amount-raised-value"]/text()')
+    goal = [item.replace("\n", "") for item in goal]
+    #This prints the currently raised amount out of the total goal
+    with open("donation.txt", "w") as text_file:
+        print("".join(map(str, goal)), '/$10,000', sep="", file=text_file)
+timeloop()
